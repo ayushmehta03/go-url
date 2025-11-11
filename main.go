520b72899ea1f56f93bd06042bc15a9e660b300a
Ayush Mehta <ayushmehtabps@gmail.com>
2025-11-11 21:35:41 +0530

    func

--- main.go
+++ main.go
@@ -1,7 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"time"
+)
+
+
+type Url struct{
+	Id string `json:"id"`
+	OrignalUrl string `json:"orignal_url"`
+	ShortUrl string `json:"short_url"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+var urlDb=make(map[string] Url)
+
+func generateShortUrl(OrignalURL string) string{
+
+
+	// intilaize the md5 module
+	hahser:=md5.New()
+
+
+	// pass the orignal url by converting into bytes cause hashing algorithm inly works for bytes
+	hahser.Write([] byte(OrignalURL))
+
+
+	//  give the final bytes
+	data:=hahser.Sum(nil)
+
+
+	// convert into readable hexadecimal string
+
+	hash:=hex.EncodeToString(data);
+
+
+	fmt.Println(hash[:8])
+
+
+	return "ayush"
+}
+
+
+func createURL( OrignalURL string) string {
+	shortUrl:=generateShortUrl(OrignalURL)
+
+	id:=shortUrl
+
+	urlDb[id]=Url{
+		Id: id,
+		OrignalUrl: OrignalURL,
+		ShortUrl: shortUrl,
+		CreatedAt: time.Now(),
+	}
+
+	return shortUrl;
+
+}
+
+
 
 func main(){
 	fmt.Println("intial")
+
 }
